501f494d75fc58cea0acb6a0930d09733ddb5ba5
ricky-kiva <ricky.kiva@gmail.com>
2025-09-12 01:03:24 +0700

    feat(gql): impl. `author` param. on `allBooks` query

--- graphql/resolvers/queryRsv.ts
+++ graphql/resolvers/queryRsv.ts
@@ -2,12 +2,18 @@ import authors from '../../data/authors';
 import books from '../../data/books';
 import { Author } from '../../types/Author';
 import { Book } from '../../types/Book';
+import { AllBooksArgs } from './args-types/queryRsvArgs';
 
 const allAuthors = (): Author[] => {
   return authors;
 };
 
-const allBooks = (): Book[] => {
+const allBooks = (_: unknown, args: AllBooksArgs): Book[] => {
+  if (args.author) {
+    const search = args.author.toLowerCase();
+    return books.filter((book) => book.author.toLowerCase().includes(search));
+  }
+
   return books;
 };
 
